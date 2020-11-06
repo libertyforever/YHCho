e679531feaf6f764b733b1d34fbffc0c8277d7e7
YHCho <hyeong5273pg@gmail.com>
2020-11-06 11:46:54 +0900

    최초 커밋 테스트

--- testRepo/src/firstPackage/FirstClass.java
+++ testRepo/src/firstPackage/FirstClass.java
@@ -3,7 +3,8 @@ package firstPackage;
 public class FirstClass {
 
 	public static void main(String[] args) {
-		System.out.println("테스트 완료");
+		System.out.println("깃 연결 및 테스트 완료");
+		System.out.println("깃 push and commit");
 	}
 
 }
